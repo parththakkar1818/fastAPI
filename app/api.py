f592be29738bae7d63f8c0c952e096957c8d59f3
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-02-04 12:41:35 +0530

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,7 +1,31 @@
+from typing import Union
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/",tags=["Root"])
-async def hello():
-    return{"hello": "sucess"}
+# Allow all origins for simplicity. In production, you should specify the allowed origins explicitly.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # replace with the list of allowed origins if you know them
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/sum")
+def calculate_sum(num1: int, num2: int):
+    result = num1 + num2
+    return {"result": result}
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/",tags=["Root"])
+# async def hello():
+#     return{"hello": "sucess"}
